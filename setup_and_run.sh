6a9f13564e17bbc98b21d0f4028858ac2f2ffe7c
Moa Ahlberg <moa.ahlberg@knowit.se>
2024-09-30 14:21:42 +0200

    Fixed the bash script

--- setup_and_run.sh
+++ setup_and_run.sh
@@ -2,31 +2,37 @@
 
 # Define the Python file to run
 PYTHON_FILE="start.py" # Replace with your actual Python file name
+VENV_DIR=".venv"
 
-# Create a virtual environment
-echo "Creating a virtual environment..."
-python3 -m venv venv
+sudo apt update
 
-# Activate the virtual environment
-echo "Activating the virtual environment..."
-source venv\Scripts\activate
-
-# Install packages from requirements.txt
-if [[ -f "requirements.txt" ]]; then
-    echo "Installing packages from requirements.txt..."
-    python3 -m pip install -r requirements.txt
-
+# Check if the virtual environment directory exists
+if [ -d "$VENV_DIR" ]; then
+    echo "Virtual environment already exists."
 else
-    echo "requirements.txt not found!"
-    exit 1
+    echo "Creating virtual environment..."
+    python3 -m venv "$VENV_DIR"
+
+    echo "Virtual environment created."
 fi
+echo "Activating the virtual environment..."
+source .venv/bin/activate
+
 
 # Run docker-compose up
 echo "Starting Docker containers with docker-compose..."
 docker-compose up -d
 
+# Install packages from requirements.txt
+if [[ -f "requirements.txt" ]]; then
+    echo "Installing packages from requirements.txt..."
+    pip install -r requirements.txt
+else
+    echo "No requirements.txt found."
+fi
+
 # Run the specified Python file
-if [[ -f "$PYTHON_FILE" ]]; then
+if [ -f "$PYTHON_FILE" ]; then
     echo "Running the Python file: $PYTHON_FILE..."
     python3 $PYTHON_FILE
 else
@@ -34,8 +40,8 @@ else
     exit 1
 fi
 
-# Deactivate the virtual environment
-echo "Deactivating the virtual environment..."
-deactivate
+# # Deactivate the virtual environment
+# echo "Deactivating the virtual environment..."
+# deactivate
 
 echo "Script completed successfully."
