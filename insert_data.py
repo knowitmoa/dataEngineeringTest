dbb0f63fb39d3570d0e0b4158f2fd6d0d1648def
Moa Ahlberg <moa.ahlberg@knowit.se>
2024-10-02 14:07:56 +0200

    Started to do a join with the subtables, change the insertmethod so it goes through all the pages in the url

--- insert_data.py
+++ insert_data.py
@@ -17,35 +17,54 @@ def insert_to_tables(connection_string):
 
     
     engine = create_engine(connection_string)
-    totalDf = pd.DataFrame()
-
-    for key, url in data.items():
-        try:
-            response = requests.get(url)
-            response.raise_for_status()
-
-            data = response.json()  # Gör jsonobjektet till en dictionairy
-            # tar ut valuen som mappar till keyn 'results'
-            resultData = data['results']
-
-            # skapar en dataframe med resultData
-            df = pd.DataFrame(resultData)
-
-            totalDf = pd.concat([totalDf,df], axis=0)
-
-        except requests.exceptions.HTTPError as http_err:
-            print(f"HTTP error occurred: {http_err}")
-        except Exception as err:
-            print(f"An error occurred: {err}")
-
-    totalDf.to_sql('starWarsTable', con=engine, if_exists='replace', index=False)
-
-    print('starWarsTable was created successfully!')
     
 
+    for key, url in data.items():
+        for i in range(1,10):
+            try:
+                pageUrl= url+"?page="+str(i)
+                print(pageUrl)
+                response = requests.get(pageUrl)
+                response.raise_for_status()
 
-    engine.dispose()
+                data = response.json()  # Gör jsonobjektet till en dictionairy
+                print(data)
+                # tar ut valuen som mappar till keyn 'results'
+                resultData = data['results']
 
+                # skapar en dataframe med resultData
+                df = pd.DataFrame(resultData)
+                df.to_sql(key, con=engine, if_exists='append', index=False)
+                print(  f"{key} was created successfully!")
+
+            
+
+            except requests.exceptions.HTTPError as http_err:
+                print(f"HTTP error occurred: {http_err}")
+            except Exception as err:
+                print(f"An error occurred: {err}")
+        
+    
+
+    
+        engine.dispose()
+
+def create_one_big_table(connectionString):
+  
+  engine = create_engine(connectionString)
+
+  people_planets_df = pd.read_sql( '''SELECT people.name AS people_name, planets.name AS planets_name FROM people FULL OUTER JOIN planets ON people.films=planets.films;''', engine)
+  
+
+  people_planets_df.to_sql('total', con=engine , if_exists='replace', index=False)
+
+  engine.dispose()
+
+
+
+    
+
+   
 
 
 
--- start.py
+++ start.py
@@ -7,8 +7,9 @@ from insights import *
 connectionString = 'postgresql://postgres_user:postgres_password@localhost:5432/starwars'
 
 insert_to_tables(connectionString)
+create_one_big_table(connectionString)
 
-insights(connectionString)
+# insights(connectionString)
 
 
 
